e639f76d975da3abe0d04dd2b8aa378d4c627a39
Ben Christel <bchristel@pivotal.io>
2020-04-22 10:36:07 -0700

    Add reproduction

--- bacteria/index.js
+++ bacteria/index.js
@@ -2,6 +2,11 @@ const canvas = document.getElementById('canvas')
 const canvasWidth = canvas.width
 const canvasHeight = canvas.height
 const ctx = canvas.getContext('2d')
+const camera = {
+  zoom: 0.8,
+  x: canvasWidth / 2,
+  y: canvasHeight / 2,
+}
 
 /* DO NOT TOUCH THIS CODE OR YOU WILL BE FIRED */
 /* vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv */
@@ -24,21 +29,34 @@ function view(state) {
 
 function updateState(state) {
   return $(state, atPath('circles', circles => {
-    return circles.map(circle => {
-      const dx = (Math.random() - 0.5) / circle.radius * 50
-      const dy = (Math.random() - 0.5) / circle.radius * 50
+    return circles.flatMap(circle => {
+      if (circle.radius < 1) return []
+
+      const dx = Math.cos(circle.direction) / (10 + circle.radius) * 50
+      const dy = Math.sin(circle.direction) / (10 + circle.radius) * 50
       const distanceMoved = Math.sqrt(square(dx) + square(dy))
-      return {
-        ...circle,
-        radius: Math.max(0, 0
-          + circle.radius
-          + intersectedMines(circle, state.mines) * efficiency(circle)
-          - death(circle)
-          - battleDamage(circle, circles)),
-        life: circle.life - distanceMoved * 1,
-        x: circle.x + dx,
-        y: circle.y + dy,
-      }
+      const givesBirth = circle.radius > 10 && Math.random() < 0.002 * distanceMoved
+      const newRadius = Math.max(0, 0
+        + circle.radius
+        + intersectedMines(circle, state.mines) * efficiency(circle)
+        - death(circle)
+        - battleDamage(circle, circles))
+        - (givesBirth ? circle.radius * 0.2 : 0)
+
+      const dradius = newRadius - circle.radius
+      const newDirection = circle.direction + (dradius * 0.2)
+
+      return [
+        {
+          ...circle,
+          direction: newDirection,
+          radius: newRadius,
+          life: circle.life - distanceMoved,
+          x: circle.x + dx,
+          y: circle.y + dy,
+        },
+        ...maybeChild(givesBirth, circle)
+      ]
     })
   }))
 }
@@ -49,6 +67,7 @@ function initialState() {
       return {
         x: Math.random() * 800,
         y: Math.random() * 800,
+        direction: randomAngle(),
         life: 2000, // + Math.random() * 1000,
         radius: Math.random() * 100,
         color: randomColor(),
@@ -63,6 +82,26 @@ function initialState() {
   }
 }
 
+function maybeChild(givesBirth, circle) {
+  if (!givesBirth) return []
+
+  const angle = randomAngle()
+  return [
+    {
+      x: circle.x + Math.cos(angle) * circle.radius * 1.2,
+      y: circle.y + Math.sin(angle) * circle.radius * 1.2,
+      direction: angle,
+      life: 2000, // + Math.random() * 1000,
+      radius: circle.radius * 0.2,
+      color: circle.color,
+    }
+  ]
+}
+
+function randomAngle() {
+  return Math.random() * 2 * Math.PI
+}
+
 function intersectedMines(circle, mines) {
   return mines.reduce((count, mine) => {
     if (square(circle.x - mine.x) + square(circle.y - mine.y) <= square(circle.radius)) {
@@ -73,17 +112,22 @@ function intersectedMines(circle, mines) {
 }
 
 function death({life}) {
-  return life < 0 ? 1 : 0
+  return life < 0 ? life * -0.01 : 0
 }
 
 function battleDamage(warrior, enemies) {
   return enemies.reduce((total, enemy) => {
-    if (!circlesIntersect(warrior, enemy)) {
-      return total + 0.01
+    if (ratio(warrior.radius, enemy.radius) < 3 && circlesIntersect(warrior, enemy)) {
+      return total + 0.1
     } else {
       return total
     }
   }, 0)
+}
+
+function ratio(a, b) {
+  if (a > b) return a / b
+  return b / a
 }
 
 function efficiency({radius}) {
@@ -127,22 +171,22 @@ function render(view) {
 }
 
 function drawCircle(ctx, center, radius, color) {
-  ctx.beginPath()
-  ctx.strokeStyle = color
-  ctx.arc(center[0], center[1], radius, 0, 2 * Math.PI, false)
-  ctx.stroke()
+  return drawArc(ctx, center, radius, color, 1)
 }
 
 function drawArc(ctx, center, radius, color, fraction) {
+  const [x, y] = cameraTransformPoint(center)
   ctx.beginPath()
   ctx.strokeStyle = color
-  ctx.arc(center[0], center[1], radius * 0.99, 0, 2 * Math.PI * fraction, false)
+  ctx.arc(x, y, cameraTransformScalar(radius), 0, 2 * Math.PI * fraction, false)
   ctx.stroke()
 }
 
 function drawSquare(ctx, center, width, color) {
+  const [x, y] = cameraTransformPoint(center)
   ctx.fillStyle = color
-  ctx.fillRect(center[0] - width / 2, center[1] - width / 2, width, width)
+  const offset = cameraTransformScalar(width / 2)
+  ctx.fillRect(x - offset, y - offset, cameraTransformScalar(width), cameraTransformScalar(width))
 }
 
 function randomColor() {
@@ -151,6 +195,18 @@ function randomColor() {
     .map(_ => _.toString(16))
     .join('')
 }
+
+function cameraTransformPoint([x, y]) {
+  return [
+    (x - camera.x) * camera.zoom + canvasWidth / 2,
+    (y - camera.y) * camera.zoom + canvasWidth / 2
+  ]
+}
+
+function cameraTransformScalar(x) {
+  return x * camera.zoom
+}
+
 
 function arrayOfLength(length, contents=1) {
   const result = []
